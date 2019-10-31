b1b4663ec5a2d896eb1ea7b70ae4b8cef358e007
田原 一樹 <ssp0727.lnc@gmail.com>
2019-10-31 23:41:05 +0900

    update

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,15 @@
-const About = () => (
-  <h1>Hello About Page</h1>
-)
+// const About = () => (
+//   <h1>Hello About Page</h1>
+// )
+
+// export default About
+
+class About extends React.Component {
+  render () {
+    return (
+      <h1>Hello About Page</h1>     
+    )
+  }
+}
 
 export default About
