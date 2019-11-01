766603f2ebdeb66f4bd083bd2a936985e942c046
田原 一樹 <ssp0727.lnc@gmail.com>
2019-11-01 23:04:29 +0900

    update

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,4 +1,18 @@
+import { useState } from 'react'
+
 const SideMenu = () => {
+  const [ count, setCount ] = useState(0)
+
+  const increment = () => {
+    const newCount = count + 1
+    setCount(newCount)
+  }
+
+  const decrement = () => {
+    const newCount = count - 1
+    setCount(newCount)
+  }
+
   return (
     <div>
       <h1 className="my-4">Shop Name</h1>
@@ -6,6 +20,11 @@ const SideMenu = () => {
         <a href="#" className="list-group-item">Category 1</a>
         <a href="#" className="list-group-item">Category 2</a>
         <a href="#" className="list-group-item">Category 3</a>
+      </div>
+      <div>
+        <button onClick={increment} className="btn btn-primary">Increment Number</button>
+        <button onClick={decrement} className="btn btn-primary">Decrement Number</button>
+        <h1>{count}</h1>
       </div>
     </div>
   )
