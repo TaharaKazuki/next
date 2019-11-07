9478246f56cfefedb3e0549d966bd61bfdbc4854
田原 一樹 <ssp0727.lnc@gmail.com>
2019-11-08 00:04:26 +0900

    update

--- actions/index.js
+++ actions/index.js
@@ -37,7 +37,7 @@ export const getMovies = () => {
   })
 }
 
-export const getMovieById = () => {
+export const getMovieById = (id) => {
   return new Promise((resolve, reject) => {
     const movieIndex = MOVIE_DATA.findIndex(m => m.id === id)
     const movie = MOVIE_DATA[movieIndex]
